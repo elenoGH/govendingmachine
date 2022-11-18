8e93197132d46479b8507256d109da8b05f8efcf
goeleno <goeleno@gmail.com>
2022-11-18 17:34:15 -0600

    se agrega funcionalidad Nested RecyclerVew para poner listado de items y listados parecidos a los de Play Store

---
+++ app/src/main/java/com/mku/govendingmachine/MainActivity.java
@@ -0,0 +1,197 @@
+package com.mku.govendingmachine;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowInsets;
+import android.view.WindowManager;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mku.govendingmachine.databinding.ActivityFullscreenBinding;
+import com.mku.govendingmachine.databinding.ActivityMainBinding;
+import com.mku.govendingmachine.entity.ChildItem;
+import com.mku.govendingmachine.entity.ParentItem;
+import com.mku.govendingmachine.service.ParentItemAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+    private static final int UI_ANIMATION_DELAY = 300;
+    private List<ChildItem> ChildItemList;
+    private boolean mVisible;
+    private View mContentView;
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        mVisible = true;
+        mContentView = binding.parentRecyclerview;
+        RecyclerView
+                ParentRecyclerViewItem
+                = findViewById(
+                R.id.parent_recyclerview);
+
+        // Initialise the Linear layout manager
+        LinearLayoutManager
+                layoutManager
+                = new LinearLayoutManager(
+                MainActivity.this);
+
+        // Pass the arguments
+        // to the parentItemAdapter.
+        // These arguments are passed
+        // using a method ParentItemList()
+        ParentItemAdapter
+                parentItemAdapter
+                = new ParentItemAdapter(
+                ParentItemList());
+
+        // Set the layout manager
+        // and adapter for items
+        // of the parent recyclerview
+        ParentRecyclerViewItem
+                .setAdapter(parentItemAdapter);
+        ParentRecyclerViewItem
+                .setLayoutManager(layoutManager);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        Log.i("Action: ", "onPostCreate");
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+    private List<ParentItem> ParentItemList()
+    {
+        List<ParentItem> itemList
+                = new ArrayList<>();
+
+        ParentItem item
+                = new ParentItem(
+                "Title 1",
+                ChildItemList());
+        itemList.add(item);
+        ParentItem item1
+                = new ParentItem(
+                "Title 2",
+                ChildItemList());
+        itemList.add(item1);
+        ParentItem item2
+                = new ParentItem(
+                "Title 3",
+                ChildItemList());
+        itemList.add(item2);
+        ParentItem item3
+                = new ParentItem(
+                "Title 4",
+                ChildItemList());
+        itemList.add(item3);
+
+        return itemList;
+    }
+
+    // Method to pass the arguments
+    // for the elements
+    // of child RecyclerView
+    private List<ChildItem> ChildItemList()
+    {
+        List<ChildItem> ChildItemList
+                = new ArrayList<>();
+
+        ChildItemList.add(new ChildItem("Card 1"));
+        ChildItemList.add(new ChildItem("Card 2"));
+        ChildItemList.add(new ChildItem("Card 3"));
+        ChildItemList.add(new ChildItem("Card 4"));
+
+        return ChildItemList;
+    }
+    /**
+     * Schedules a call to hide() in delay milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        Log.i("Action: ", "delayedHide");
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+    private final Handler mHideHandler = new Handler(Looper.myLooper());
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            Log.i("Action: ", "mHideRunnable");
+            hide();
+        }
+    };
+    private void hide() {
+        // Hide UI first
+        ActionBar actionBar = getSupportActionBar();
+        Log.i("Action: ", "hide");
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+        //mControlsView.setVisibility(View.GONE);
+        mVisible = false;
+
+        // Schedule a runnable to remove the status and navigation bar after a delay
+        mHideHandler.removeCallbacks(mShowPart2Runnable);
+        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);//ejecusion de proceso en sierto tiempo
+    }
+    private final Runnable mShowPart2Runnable = new Runnable() {
+        @Override
+        public void run() {
+            Log.i("Action: ", "mShowPart2Runnable");
+            // Delayed display of UI elements
+            ActionBar actionBar = getSupportActionBar();
+            if (actionBar != null) {
+                actionBar.show();
+            }
+            //mControlsView.setVisibility(View.VISIBLE);
+        }
+    };
+    private final Runnable mHidePart2Runnable = new Runnable() {
+        @SuppressLint("InlinedApi")
+        @Override
+        public void run() {
+            Log.i("Action: ", "mHidePart2Runnable");
+            // Delayed removal of status and navigation bar
+            if (Build.VERSION.SDK_INT >= 30) {
+                mContentView.getWindowInsetsController().hide(
+                        WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+            } else {
+                // Note that some of these constants are new as of API 16 (Jelly Bean)
+                // and API 19 (KitKat). It is safe to use them, as they are inlined
+                // at compile-time and do nothing on earlier devices.
+                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+            }
+        }
+    };
+}
